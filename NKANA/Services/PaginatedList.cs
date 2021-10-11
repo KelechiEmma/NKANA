567e2d81206ff3bdb02192563e335a81dfab69fd
NuelClet <ikennaezerebo@gmail.com>
2021-10-11 16:30:55 +0100

    change on paginatedList

--- NKANA/Services/PaginatedList.cs
+++ NKANA/Services/PaginatedList.cs
@@ -33,7 +33,7 @@ namespace NKANA.Services
             }
         }
 
-        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, intpageSize)
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
         {
             var count = await source.CountAsync();
             var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
